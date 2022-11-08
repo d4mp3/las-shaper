adfae226e9f3d164a75d238cb4da74799c3b17f5
Pszczoła <dpszczola@multiconsult.no>
2022-11-08 13:14:35 +0100

    refactor create_gdf_from_xyz

--- main.py
+++ main.py
@@ -62,76 +62,45 @@ def merge_xyz_files(in_path, out_path):
     new_file.close()
 
 
-def create_gdf_from_multiple_xyz(xyz_file):
-    new_data = gpd.GeoDataFrame()
-    new_data['geometry'] = None
-    xyz_file = open(xyz_file, 'r')
-    count = 0
-    dataframes = []
-
-    while True:
-        line = xyz_file.readline()
-        if not line:
-            print("END OF FILE")
-            break
-
-        line = line.split()
-        df = pd.DataFrame({'x': [float(line[0])], 'y': [
-                          float(line[1])], 'z': [float(line[2])]})
-        # geometry = gpd.points_from_xy(df['x'], df['y'], df['z'])
-        gdf = gpd.GeoDataFrame(
-            df, geometry=gpd.points_from_xy(df['x'], df['y']))
-        inside_polygon(gdf, buildings, df, dataframes)
-
-        print("Processed {} lines".format(count+1))
-        count += 1
-
-    return gpd.GeoDataFrame(pd.concat(dataframes, ignore_index=True, 
-                                      crs=dataframes[0].crs))
-
-
-def create_gdf_from_single_xyz(in_path, out_path):
+def create_gdf_from_xyz(in_path, out_path, poly):
     pathlist = Path(in_path).glob('**/*.xyz')
 
     for path in pathlist:
-        dataframes = []
-        xyz_file = open(path, 'r')
-        count = 0
-        results = []
-        
-        # print(results.crs)
-        for line in xyz_file.readlines():
-            line = line.split()
-            df = pd.DataFrame({'x': [float(line[0])], 'y': [float(line[1])],
-                               'z': [float(line[2])]})
-            gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['x'],
-                                                                   df['y']))
-        
-            a = inside_polygon(gdf['geometry'], buildings)
-            
-            if a is True:
-                dataframes.append(gdf)
-            
-            
-            if count > 1000:
-                break
-            
-            
-            print("Processed {} lines".format(count+1))
-            count += 1
 
-        return gpd.GeoDataFrame(pd.concat(dataframes, ignore_index=True))
-        break
-
-def inside_polygon(geom, poly):
-    for row in poly.iterrows():
-        if str(geom.within(row[1][13])[0]) == 'True':
-            return True
+        df = pd.read_csv(path, sep='\t', header = None)
+        df.columns = ['x', 'y', 'z']
+        points = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['x'], df['y']))
         
+        return intersect_using_spatial_index(points, poly)
+       
+
+def intersect_using_spatial_index(source_gdf, intersecting_gdf):
+    """
+    Conduct spatial intersection using spatial index for candidates GeoDataFrame to make queries faster.
+    Note, with this function, you can have multiple Polygons in the 'intersecting_gdf' and it will return all the points 
+    intersect with ANY of those geometries.
+    """
+    source_sindex = source_gdf.sindex
+    possible_matches_index = []
+    
+    # 'itertuples()' function is a faster version of 'iterrows()'
+    for other in intersecting_gdf.itertuples():
+        bounds = other.geometry.bounds
+        c = list(source_sindex.intersection(bounds))
+        possible_matches_index += c
+    
+    # Get unique candidates
+    unique_candidate_matches = list(set(possible_matches_index))
+    possible_matches = source_gdf.iloc[unique_candidate_matches]
+
+    # Conduct the actual intersect
+    result = possible_matches.loc[possible_matches.intersects(intersecting_gdf.unary_union)]
+    return result
+
         
 start_time = time.time()
-b = create_gdf_from_single_xyz(IN_XYZ, OUT_PATH)
-b.to_file('./data/shp/results_test.shp')
+b = create_gdf_from_xyz(IN_XYZ, OUT_PATH, buildings)
+# b.to_file('./data/shp/results_test.shp')
 print("--- %s seconds ---" % (time.time() - start_time))
 
 # print("Calling extract_las_classification")
@@ -140,7 +109,7 @@ print("--- %s seconds ---" % (time.time() - start_time))
 # create_xyz_from_las(trimmed_las, OUT_PATH)
 # print("Calling merge_multiple_xyz_files")
 # merge_xyz_files(IN_XYZ, OUT_PATH)
-print("Calling create_gdf_from_xyz")
+# print("Calling create_gdf_from_xyz")
 
 
 # a = inside_polygon(test_points, buildings)
